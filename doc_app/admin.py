38b7eac438149f45a0bc43f26b9fe712ca219a13
Alexey <108893923+Alexey913@users.noreply.github.com>
2023-12-19 21:44:34 +0300

    Update admin.py

--- doc_app/admin.py
+++ doc_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Spouce, Children, DriverCategoryShedule, Passport, Inn, Snils, DriverLicense, ForeignPassport, MilitaryTicket
+
+
+admin.site.register(Spouce)
+admin.site.register(Children)
+admin.site.register(DriverCategoryShedule)
+admin.site.register(Passport)
+admin.site.register(Inn)
+admin.site.register(Snils)
+admin.site.register(DriverLicense)
+admin.site.register(ForeignPassport)
+admin.site.register(MilitaryTicket)
