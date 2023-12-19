60727573f3f04191c1d8c147f3f89af0c3d29c2d
Alexey <108893923+Alexey913@users.noreply.github.com>
2023-12-19 11:40:21 +0300

    Update views.py

--- contacts_app/views.py
+++ contacts_app/views.py
@@ -296,8 +296,8 @@ def search(request, user_id):
                 organization__icontains=contact, user_id=user_id).all()
             place_rsidense_search = Contact.objects.filter(
                 place_residense__icontains=contact, user_id=user_id).all()
-            contacts = list(chain(name_search, suname_search,
-                                     patronymic_search, organization_search, place_rsidense_search))
+            contacts = list(set(chain(name_search, suname_search,
+                                     patronymic_search, organization_search, place_rsidense_search)))
             mes = 'По Вашему запросу ничего не найдено'
             title_templ = f'Найдено по запросу {contact}:'
             return abstract_list_contacts(request, user_id, contacts, mes, title_templ)
